bbfd44eca032539b2aa5561f2266353f1f64762d
Ebhosdest <ibhatedestiny@gmail.com>
2025-04-28 10:30:30 +0100

    Folder structure added

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Medical-Chatbot-Generative-AI
+### STEP 01- Create a conda environment after opening the repository
+conda create -n medicalbot python=3.10 -y
+conda activate medicalbot
+
+### STEP 02- install the requirements
+pip install -r requirements.txt
+
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Destiny Ibhate',
+    author_email= 'ibhatedestiny@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
+
+
+
+
